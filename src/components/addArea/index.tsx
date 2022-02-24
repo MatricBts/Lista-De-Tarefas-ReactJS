7c906ecf3d5a7feb3dee0986e10a8d7ae14194b8
Matric Bastos <matricbts@gmail.com>
2022-02-24 07:14:20 -0300

    fazendo mudanças

--- src/components/addArea/index.tsx
+++ src/components/addArea/index.tsx
@@ -1,5 +1,6 @@
 import { useState, KeyboardEvent } from 'react'
 import * as C from './styles'
+import { buscaStorage, salvaStorage  } from '../../types/storage';
 
 type Props = { //CRIANDO PROPS
     onEnter: ( nomeTarefa:string ) => void // DECLARA QUE ONENTERÉ UMA FUNÇÃO QUE RETORNA NADA
@@ -7,11 +8,16 @@ type Props = { //CRIANDO PROPS
 
 export const AddArea = ( {onEnter}:Props ) => {
     const [ inputText, setInputText ] = useState('');
+    let [ tese, setTese ] = useState( localStorage.getItem( 'tarefas' ) )
+
 
     const pegaTecla = (e: KeyboardEvent) => {
+
         if(e.code === 'Enter' && inputText !== ''){
             onEnter( inputText );
             setInputText('');
+            salvaStorage(inputText);
+            console.log( tese )
         }
     }
 
